c7f125766f688421cb32a6ff9c7f7f09ce67d88b
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-26 23:31:18 +0600

    redux setup  for statemanagement

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <div className=" text-4xl font-bold">Todo app</div>;
+  return (
+    <div className=" text-4xl font-bold">
+      Todo app
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
