555927a7732e0405e2b611cedd06d8969e4b7987
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-27 00:28:19 +0600

    some basic component

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const TodoCard = () => {
-  return <div>TodoCard.tsx</div>;
+  return (
+    <div className=" bg-white rounded-md flex justify-between items-center p-3">
+      <input type="checkbox" name="" id="" />
+      <p className=" font-semibold">Todo Title</p>
+      <p>Time</p>
+      <p>Description</p>
+      <div className=" space-x-5 ">
+        <button>Delete</button>
+        <button>Edit</button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoCard;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
-  return <div>TodoContainer</div>;
+  return (
+    <div>
+      <div>
+        <button>Add Todo</button>
+        <button>Filter</button>
+      </div>
+      <div className=" bg-purple-900 w-full min-h-[500px] rounded-xl p-5 space-y-10">
+        {/*  <div className=" bg-white p-5 flex justify-center items-center rounded-md">
+          <p className="text-center text-2xl font-bold">
+            There is no task pending
+          </p>
+        </div> */}
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+      </div>
+    </div>
+  );
 };
 
 export default TodoContainer;
