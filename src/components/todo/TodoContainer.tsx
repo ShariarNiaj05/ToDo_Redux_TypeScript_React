ca3ad8494b2e0f9fbf8b5b5a36fc2c0736e07008
ShariarNiaj05 <shariarn85@gmail.com>
2024-06-27 22:38:57 +0600

    Install Shadcn Components

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,14 +1,16 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import NoPendingTaskCard from "./NoPendingTaskCard";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
         <button>Filter</button>
       </div>
-      <div className=" bg-purple-900 w-full min-h-[500px] rounded-xl p-5 space-y-10">
+      <div className=" bg-primary-gradient w-full min-h-[500px] rounded-xl p-5 space-y-10">
         <NoPendingTaskCard />
         <TodoCard />
       </div>
